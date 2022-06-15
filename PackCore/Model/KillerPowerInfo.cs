a744178429cd8d360c66169d81a0a2a8bab3c40f
Toon Werawat <14814401+ray1997@users.noreply.github.com>
2022-06-16 04:27:35 +0700

    Restructure info classes

---
+++ PackCore/Model/KillerPowerInfo.cs
@@ -0,0 +1,15 @@
+﻿namespace IconPack.Model
+{
+    public class KillerPowerInfo : IBaseItemInfo
+    {
+        public KillerPowerInfo(string? name, string? owner)
+        {
+            Name = name;
+            if (owner is not null)
+                PowerOwner = owner;
+        }
+
+        public string? Name { get; set; }
+        public string? PowerOwner { get; set; }
+    }
+}
